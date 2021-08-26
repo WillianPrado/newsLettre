376fb616804c7128067c38080a499fad5d60c45a
WillianPrado <willian95prado@gmail.com>
2021-08-26 18:16:11 -0300

    Procura noticias por titulo usando like

--- src/main/java/com/br/sucessoemotivacao/newsLettrer/controllers/NewsController.java
+++ src/main/java/com/br/sucessoemotivacao/newsLettrer/controllers/NewsController.java
@@ -1,10 +1,8 @@
 package com.br.sucessoemotivacao.newsLettrer.controllers;
 
 import com.br.sucessoemotivacao.newsLettrer.dto.request.NewsDTO;
-import com.br.sucessoemotivacao.newsLettrer.dto.request.PersonDTO;
 import com.br.sucessoemotivacao.newsLettrer.dto.response.MessageResponseDTO;
 import com.br.sucessoemotivacao.newsLettrer.exception.NewsNotFoundException;
-import com.br.sucessoemotivacao.newsLettrer.exception.PersonNotFoundException;
 import com.br.sucessoemotivacao.newsLettrer.services.NewService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,29 +16,33 @@ import java.util.List;
 @RequestMapping("/api/v1/news")
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class NewsController {
-    private final NewService newService;
+    private final NewService newsService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     
     public MessageResponseDTO create(@RequestBody @Valid NewsDTO newsDTO) {
-        return newService.create(newsDTO);
+        return newsService.create(newsDTO);
     }
 
     @GetMapping
     public List<NewsDTO> listAll() {
-        return newService.listAll();
+        return newsService.listAll();
     }
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public NewsDTO findById(@PathVariable Long id) throws NewsNotFoundException {
-        return newService.findById(id);
+        return newsService.findById(id);
+    }
+    @GetMapping("titulo/{titulo}")
+    public List<NewsDTO> findByTituloContaining(@PathVariable String titulo) throws NewsNotFoundException{
+        return newsService.findByTituloContaining(titulo);
     }
 //teste
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) throws NewsNotFoundException {
-        newService.delete(id);
+        newsService.delete(id);
     }
 }
